5ed826cecb31e287d289d88db8f598efbda159d9
Natali Rak <64411166+NataliRak@users.noreply.github.com>
2023-05-03 14:29:36 +0300

    Add files via upload

--- src/constans/constans.js
+++ src/constans/constans.js
@@ -4,7 +4,7 @@ export const projects = [
     description:
       "While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions.  And trust me, e-commerce applications are impressive.",
     image: "/images/Art-Shop.png",
-    tags: ["React", "JavaScript", "Bootstrap"],
+    tags: ["React", "JavaScript", "Material UI"],
     source: "https://github.com/NataliRak/Gallery_Shop",
     visit: "https://gallery-shop.netlify.app/",
     id: 0,
@@ -20,6 +20,16 @@ export const projects = [
     id: 5,
   },
   {
+    title: "Crypto-Hunter",
+    description:
+      "Create a Cryptocurrency Tracker App in React JS, Material UI and Chart JS with full responsive fuctionality. I used Context API to manage the state of our app and Coin Gecko API for backend.",
+    image: "/images/crypto.png",
+    tags: ["Material UI", "React", "Chart JS"],
+    source: "https://github.com/NataliRak/Crypto-Hunter",
+    visit: "https://crypto-hunter-rak.netlify.app/",
+    id: 4,
+  },
+  {
     title: "Weahter-App",
     description:
       "Weather app in React JS, I use the Open Weather Map API to make calls to a restful api to return the current weather data.This uses the Modern Javascript Fetch AP along with React hooks and conditonals.",
@@ -29,16 +39,7 @@ export const projects = [
     visit: "https://weather-natali-rak.netlify.app/",
     id: 1,
   },
-  {
-    title: "To-Do List",
-    description:
-      " React Todo List App. I use React hooks, so useEffect, useState, and useRef. You can be able to add, remove, edit, and mark complete/cross out the todo items. ",
-    image: "/images/todo-app.png",
-    tags: ["Bootstrap", "React", "JavaScript"],
-    source: "https://github.com/NataliRak/react-todo-list",
-    visit: "https://react-todo-list-rak.netlify.app/",
-    id: 2,
-  },
+
   {
     title: "Portfolio Website",
     description:
@@ -50,13 +51,75 @@ export const projects = [
     id: 3,
   },
   {
-    title: "Travel Agency",
+    title: "To-Do List",
     description:
-      "Lenovo SNAPit was one of the finest OEM camera apps back in those days. It had tonnes of unique and amazing features. I ported it to work on almost all devices at that time.",
-    image: "/images/travel.jpg",
+      " React Todo List App. I use React hooks, so useEffect, useState, and useRef. You can be able to add, remove, edit, and mark complete/cross out the todo items. ",
+    image: "/images/todo-app.png",
     tags: ["Bootstrap", "React", "JavaScript"],
-    source: "https://github.com/NataliRak/Gallery_Shop",
-    visit: "https://gallery-shop.netlify.app/",
-    id: 4,
+    source: "https://github.com/NataliRak/react-todo-list",
+    visit: "https://react-todo-list-rak.netlify.app/",
+    id: 2,
+  },
+];
+export const nodeProjects = [
+  {
+    title: "E-Commerce Shop",
+    description:
+      "For back-end side i used NodeJS MongoDB eCommerce Rest API using Stripe payment method. Node.js shopping API using express, MongoDB, and JWT.",
+    image: "/images/nodejs-shop.jpg",
+    tags: ["NodeJs", "Express", "React", "JavaScript"],
+    source: "https://github.com/NataliRak/shop",
+    visit: "/images/nodejs shop.jpeg",
+    id: 7,
+  },
+  {
+    title: "Fast-Company",
+    description:
+      "For back-end side I used NodeJS(create models for a users and routing, using HTTP methods for connection with a DB), MongoDB and JWT for login.",
+    image: "/images/fast-company.png",
+    tags: ["NodeJs", "Express", "JWT", "React"],
+    source: "https://github.com/NataliRak/shop",
+    visit: "/images/fast-company.png",
+    id: 8,
+  },
+  {
+    title: "NodeJS-Blog",
+    description:
+      " I wrote a news application with support for CRUD operations and data storage in MongoDB, created my own API for returning data from the database",
+    image: "/images/nodeJS_Blog.png",
+    tags: ["NodeJs", "Express", "REST API", "MONGO DB"],
+    source: "https://github.com/NataliRak/NodeJS-Blog",
+    visit: "/images/nodeJS_Blog.png",
+    id: 9,
+  },
+];
+
+export const phpProjects = [
+  {
+    title: "Clining website",
+    description: "Building a multi-page site using PHP and Wordpress",
+    image: "/images/clining.png",
+    tags: ["PHP", "Wordpress"],
+    source: "https://github.com/NataliRak/shop",
+    visit: "https://code.esek.site/Nataliya/",
+    id: 10,
+  },
+  {
+    title: "Justica",
+    description: "Building a multi-page site using PHP and Wordpress",
+    image: "/images/justica.png",
+    tags: ["PHP", "Wordpress"],
+    source: "https://github.com/NataliRak/shop",
+    visit: "http://code.esek.site/rak/",
+    id: 11,
+  },
+  {
+    title: "Lawers",
+    description: "Building a multi-page site using PHP and Wordpress",
+    image: "/images/lawer.png",
+    tags: ["PHP", "Wordpress"],
+    source: "https://github.com/NataliRak/shop",
+    visit: "https://lawyer.esek.site/",
+    id: 9,
   },
 ];
